7280ad95d512beb0b788308349b28a8924f224ce
lamcw <cw.lam@outlook.com>
2017-09-22 14:11:32 +1000

    now able to parse collection.txt and url?.txt

--- parser.c
+++ parser.c
@@ -11,21 +11,38 @@ struct handle {
 } handle;
 
 static void add_size(handle_t);
+static FILE *open_file(char *, char *);
+static handle_t new_handle(void);
 
-handle_t parse(char *path)
+// function wrapper around fopen
+static FILE *open_file(char *path, char *mode)
 {
 	FILE *fp;
-	fp = fopen(path, "r");
+	fp = fopen(path, mode);
 
 	if (fp == NULL) {
 		fprintf(stderr, "No such file: %s\n", path);
 		exit(1);
 	}
 
+	return fp;
+}
+
+static handle_t new_handle(void)
+{
 	handle_t h = (handle_t)malloc(sizeof(struct handle));
 	assert(h);
 	h->buf = NULL;
 	h->size = 0;
+
+	return h;
+}
+
+handle_t parse(char *path)
+{
+	FILE *fp = open_file(path, "r");
+
+	handle_t h = new_handle();
 	// malloc buffer
 	add_size(h);
 
@@ -36,6 +53,44 @@ handle_t parse(char *path)
 	h->size--;
 	fclose(fp);
 	return h;
+}
+
+handle_t parse_url(char *path, char *start_tag, char *end_tag)
+{
+	FILE *fp = open_file(path, "r");
+
+	handle_t h = new_handle();
+	// malloc buffer
+	add_size(h);
+
+	char *buf;
+	int read_buf= 0;
+
+	// %m[^\n]\n reads whole line until '\n' (not including \n)
+	while (fscanf(fp, "%m[^\n]\n", &buf) != EOF) {
+		// start reading next iteration
+		if (strcmp(buf, end_tag) == 0) read_buf = !read_buf;
+		if (read_buf) {
+			// use space as delimiter
+			char *token = strtok(buf, " ");
+
+			while (token != NULL) {
+				h->buf[h->size - 1] = (char *)malloc(strlen(token) + 1);
+				strcpy(h->buf[h->size - 1], token);
+				add_size(h);
+				token = strtok(NULL, " ");
+			}
+		}
+		// stop reading next iteration
+		if (strcmp(buf, start_tag) == 0) read_buf = !read_buf;
+		free(buf);
+	}
+
+	// reduce size
+	h->size--;
+	fclose(fp);
+	return h;
+
 }
 
 static void add_size(handle_t h)
